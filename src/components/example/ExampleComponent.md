8605a0087c46daa996e33fe19310e3c39777a75d
toyjhlee <toyjhlee@gmail.com>
2020-12-10 00:23:03 +0900

    Add DropDown

--- src/components/example/ExampleComponent.md
+++
@@ -1,7 +0,0 @@
-### Example
-
-```js
-import React, {useState} from 'react'
-import ExampleComponent from './ExampleComponent'
-;<ExampleComponent text="1" />
-```
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,3 @@
-import ExampleComponent from './components/example/ExampleComponent'
+import DropDown from './components/navigation/DropDown'
 
-// type Props = {text: string}
-
-// function ExampleComponent(props: Props): React.ReactElement {
-//   const {text} = props
-
-//   return <div style={{color: 'red'}}>Hello {text}</div>
-// }
-
-// class ExampleComponent extends React.Component<Props> {
-//   render() {
-//     const {text} = this.props
-
-//     return <div style={{color: 'red'}}>Hello {text}</div>
-//   }
-// }
-
-export default {ExampleComponent}
+export default {DropDown}
